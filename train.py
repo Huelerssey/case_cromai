a59708c1caba2019ae2d0f3ca46db5953b89557f
Huelerssey Rodrigues <huelersseyrd@hotmail.com>
2023-09-04 00:19:15 -0300

    feat: V4 Model
    + Implemented data augmentation techniques
    
    + Increased image resolution grid
    
    + Added early stopping callback for training monitoring
    
    + Integrated transfer learning with MobileNetV2
    
    - Removed max pooling layers due to transfer learning integration
    
    - Decreased learning rate for smoother convergence

--- train.py
+++ train.py
@@ -1,21 +1,21 @@
 import os
 import re
-import cv2 # OpenCV ou cv2 para tratamento de imagens;
 import pandas as pd
-import numpy as np # Numpy para trabalharmos com matrizes n-dimensionais
-from keras.models import Sequential # Importando modelo sequencial
-from keras.layers.convolutional import Conv2D, MaxPooling2D # Camada de convolução e max pooling
-from keras.layers.core import Activation, Flatten, Dense # Camada da função de ativação, flatten, entre outros
-from keras.layers import Rescaling, Dropout # Camada de escalonamento
-from keras.optimizers import Adam # optimizador Adam
-from keras.callbacks import ModelCheckpoint # Classe utilizada para acompanhamento durante o treinamento onde definimos os atributos que serão considerados para avaliação
-from keras.preprocessing.image import ImageDataGenerator # Gerador de imagens
-from keras.utils import image_dataset_from_directory # Função que carrega o dataset de um diretório
+import tensorflow as tf
+from keras.models import Sequential
+from keras.layers.convolutional import Conv2D
+from keras.layers.core import Activation, Flatten, Dense
+from keras.layers import Rescaling, Dropout
+from keras.optimizers import Adam
+from keras.callbacks import ModelCheckpoint, EarlyStopping
+from keras.utils import image_dataset_from_directory
 from tensorflow.data import AUTOTUNE
 
 
-# controla a versão do código
+# Controla a versão do código
 def get_latest_version(models_path):
+    """Busca a última versão do modelo no diretório fornecido."""
+
     # Lista todos os arquivos no diretório de modelos
     files = os.listdir(models_path)
     
@@ -36,30 +36,34 @@ def create_lenet(input_shape):
     Returns:
         Um modelo sequencial, seguindo a arquitetura lenet
     """
+
+    # Carrega o modelo MobileNetV2 sem as camadas superiores e com pesos pré-treinados do ImageNet
+    baseModel = tf.keras.applications.MobileNetV2(input_shape=input_shape, include_top=False, weights='imagenet')
+
     # Definimos que estamos criando um modelo sequencial
     model = Sequential()
+
+    # Adiciona o modelo base
+    model.add(baseModel)
 
     # Primeira camada do modelo:
     model.add(Conv2D(32, (3, 3), padding="same", input_shape=input_shape))
     model.add(Activation("relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(0.20))
 
     # Segunda camada do modelo:
     model.add(Conv2D(64, (3, 3), padding="same"))
     model.add(Activation("relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(0.20))
 
     # Terceira camada do modelo:
     model.add(Conv2D(128, (3, 3), padding="same"))
     model.add(Activation("relu"))
-    model.add(MaxPooling2D(pool_size=(2, 2), strides=(2, 2)))
     model.add(Dropout(0.20))
 
     # Primeira camada fully connected
     model.add(Flatten())
-    model.add(Dense(510))
+    model.add(Dense(512))
     model.add(Activation("relu"))
     model.add(Dropout(0.5))
 
@@ -70,35 +74,57 @@ def create_lenet(input_shape):
 
 
 if __name__ == "__main__":
-    train_path = "dataset/cats_and_dogs" # Adicione aqui o caminho para chegar no diretório que contém as imagens de treino na sua maquina
-    models_path = "models" # Defina aqui onde serão salvos os modelos na sua maquina
-    width = 64 # Tamanho da largura da janela que será utilizada pelo modelo
-    height = 64 # Tamanho da altura da janela que será utilizada pelo modelo
-    depth = 3 # Profundidade das janelas utilizadas pelo modelo, caso seja RGB use 3, caso escala de cinza 1
-    classes = 2 # Quantidade de classes que o modelo utilizará
-    epochs = 10 # Quantidade de épocas (a quantidade de iterações que o modelo realizará durante o treinamento)
-    init_lr = 1e-3 # Taxa de aprendizado a ser utilizado pelo optimizador
-    batch_size = 32 # Tamanho dos lotes utilizados por cada epoca
-    input_shape = (height, width, depth) # entrada do modelo
-    latest_version = get_latest_version(models_path) # adiciona um versionador de modelo antes de salvar
-    save_model = os.path.join(models_path, f"lenet-{{epoch:02d}}-{{accuracy:.3f}}-{{val_accuracy:.3f}}-V{latest_version + 1}.model") # salva o modelo versionado
-    color_mode = {1:"grayscale", 3: "rgb"} # Usado para selecionar o colormode em função da variável depth
 
+    # Caminho para chegar no diretório que contém as imagens de treino
+    train_path = "dataset/cats_and_dogs"
+
+    # Diretório onde os modelos serão salvos
+    models_path = "models"
+
+    # Tamanho da largura da janela que será utilizada pelo modelo
+    width = 128
+
+    # Tamanho da altura da janela que será utilizada pelo modelo
+    height = 128
+
+    # Profundidade das janelas utilizadas pelo modelo (RGB: 3, escala de cinza: 1)
+    depth = 3
+
+    # Quantidade de classes que o modelo utilizará
+    classes = 2
+
+    # Quantidade de épocas (iterações do modelo durante o treinamento)
+    epochs = 10
+
+    # Taxa de aprendizado para o otimizador
+    init_lr = 1e-4
+
+    # Tamanho dos lotes utilizados por cada época
+    batch_size = 32
+
+    # Forma de entrada do modelo
+    input_shape = (height, width, depth) 
+
+    # Adiciona um versionador de modelo antes de salvar
+    latest_version = get_latest_version(models_path) 
+
+    # Salva o modelo versionado
+    save_model = os.path.join(models_path, f"lenet-{{epoch:02d}}-{{accuracy:.3f}}-{{val_accuracy:.3f}}-V{latest_version + 1}.model") 
+
+    # Seleciona o modo de cor em função da variável depth
+    color_mode = {1: "grayscale", 3: "rgb"} 
+
+    # Cria o diretório dos modelos, se ele não existir
     os.makedirs(models_path, exist_ok=True)
 
-    # gera mais imagens para implementar o dataset
-    datagen = ImageDataGenerator(
-                            rotation_range=20,
-                            zoom_range=0.15,
-                            width_shift_range=0.2,
-                            height_shift_range=0.2,
-                            shear_range=0.15,
-                            horizontal_flip=True,
-                            vertical_flip=True,
-                            fill_mode="nearest",
-                            brightness_range=[0.5, 1.5]
-    )
+    # Definição de técnicas de augmentação de dados
+    data_augmentation = tf.keras.models.Sequential([
+                            tf.keras.layers.experimental.preprocessing.RandomFlip('horizontal'),
+                            tf.keras.layers.experimental.preprocessing.RandomRotation(0.2),
+                            tf.keras.layers.experimental.preprocessing.RandomZoom(0.2)
+    ])
 
+    # Criação do conjunto de dados de treinamento
     train_ds = image_dataset_from_directory(
                             train_path,
                             seed=123,
@@ -107,9 +133,11 @@ if __name__ == "__main__":
                             subset="training",
                             color_mode=color_mode[depth],
                             image_size=(height, width),
-                            batch_size=batch_size
+                            batch_size=batch_size,
+                            shuffle=True
     )
 
+    # Criação do conjunto de dados de validação
     val_ds = image_dataset_from_directory(
                             train_path,
                             seed=123,
@@ -118,28 +146,37 @@ if __name__ == "__main__":
                             subset="validation",
                             color_mode=color_mode[depth],
                             image_size=(height, width),
-                            batch_size=batch_size
+                            batch_size=batch_size,
+                            shuffle=True
     )
 
-
+    # Camada de reescalonamento para normalizar os pixels das imagens
     rescaling_layer = Rescaling(1./255)
-    # pré-busca em buffer para que você possa produzir dados do disco sem que a E/S se torne um bloqueio
+
+    # Aplica o reescalonamento aos conjuntos de treinamento e validação
     train_ds = train_ds.map(lambda x, y: (rescaling_layer(x), y), num_parallel_calls=AUTOTUNE)
     val_ds = val_ds.map(lambda x, y: (rescaling_layer(x), y), num_parallel_calls=AUTOTUNE)
 
+    # Criação do modelo
     model = create_lenet(input_shape)
 
+    # Definição do otimizador
     opt = Adam(lr=init_lr, decay=init_lr / epochs)
 
+    # Compilação do modelo
     model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=["accuracy"])
     model.summary()
 
+    # Início do treinamento
     print("\n training network")
 
+    # Definição de callbacks para o treinamento
     checkpoint1 = ModelCheckpoint(save_model, monitor='val_loss', verbose=1, save_best_only=True, mode='min')
     checkpoint2 = ModelCheckpoint(save_model, monitor='val_accuracy', verbose=1, save_best_only=True, mode='max')
-    callbacks_list = [checkpoint1,checkpoint2]
+    early_stopping = EarlyStopping(monitor='loss', patience=3)
+    callbacks_list = [checkpoint1, checkpoint2, early_stopping]
 
+    # Treinamento do modelo
     H = model.fit(train_ds,
                 validation_data=val_ds,
                 epochs=epochs, 
@@ -157,7 +194,7 @@ if __name__ == "__main__":
 
     # cria o novo DataFrame com os resultados atuais
     results_df = pd.DataFrame(H.history)
-    results_df['experiment_description'] = "V3: Inclusao de camadas de dropout para mitigar o overfitting e promover a generalizacao. Aumento de dados com tecnicas como zoom e rotacao para enriquecer o conjunto de treinamento e melhorar a robustez. Reducao da resolucao da imagem para 64x64 para acelerar o treinamento e focar em caracteristicas mais genericas. Ajuste na divisao de treino/teste para 80/20, permitindo mais dados para treinamento."
+    results_df['experiment_description'] = f"V{latest_version + 1} Adicao de Data Augmentation para enriquecer o conjunto de treinamento e melhorar a robustez do modelo. Aumento na resolucao da imagem para 128x128 para capturar mais detalhes e caracteristicas. Reducao na taxa de aprendizado para promover uma convergencia mais suave e evitar oscilacoes durante o treinamento. Integracao de Transfer Learning com MobileNetV2 para aproveitar caracteristicas aprendidas em conjuntos de dados maiores e melhorar a eficiencia. Remocao de camadas de MaxPooling devido ao uso de Transfer Learning e para evitar reducoes adicionais nas dimensoes da imagem. Inclusao de callback de Early Stopping para monitorar o treinamento e parar quando nao houver melhorias significativas."
 
     # Concatena os antigos resultados com os novos
     final_results = pd.concat([old_results, results_df], ignore_index=True)
